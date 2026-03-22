91502efb32384ef4869b552c50a0f6ee0ee32174
M1cheel <3467884929@qq.com>
2026-03-23 00:24:48 +0800

    添加文章分类的接口

---
+++ src/main/java/com/xixizai/personalblogwebsite/exception/AddFailsureException.java
@@ -0,0 +1,13 @@
+package com.xixizai.personalblogwebsite.exception;
+
+public class AddFailsureException extends Exception{
+
+    public AddFailsureException(){
+
+    }
+
+    public AddFailsureException(String msg){
+        super(msg);
+    }
+
+}
--- src/main/java/com/xixizai/personalblogwebsite/service/ArticleCategoryService.java
+++ src/main/java/com/xixizai/personalblogwebsite/service/ArticleCategoryService.java
@@ -1,5 +1,7 @@
 package com.xixizai.personalblogwebsite.service;
 
+import com.xixizai.personalblogwebsite.exception.AddFailsureException;
+import com.xixizai.personalblogwebsite.pojo.dto.ArticleCategoryDTO;
 import com.xixizai.personalblogwebsite.pojo.result.Result;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,7 @@ public interface ArticleCategoryService {
 
     //获取所有文章分类
     Result getAllArticleCategories();
+
+    //添加文章分类
+    Result addArticleCategories(ArticleCategoryDTO articleCategoryDTO) throws AddFailsureException;
 }
