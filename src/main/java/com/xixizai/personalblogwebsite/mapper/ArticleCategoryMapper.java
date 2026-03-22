9c18d5942e7d9e2f44412921963dba41b3625061
M1cheel <3467884929@qq.com>
2026-03-23 00:08:47 +0800

    获取所有文章分类

--- src/main/java/com/xixizai/personalblogwebsite/mapper/ArticleCategoryMapper.java
+++ src/main/java/com/xixizai/personalblogwebsite/mapper/ArticleCategoryMapper.java
@@ -1,7 +1,15 @@
 package com.xixizai.personalblogwebsite.mapper;
 
+import com.xixizai.personalblogwebsite.pojo.entity.ArticleCategories;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ArticleCategoryMapper {
+    //获取所有文章分类
+    @Select("select * from article_categories")
+    List<ArticleCategories> getAllArticleCategories();
+
 }
--- src/main/java/com/xixizai/personalblogwebsite/service/ArticleCategoryService.java
+++ src/main/java/com/xixizai/personalblogwebsite/service/ArticleCategoryService.java
@@ -1,7 +1,11 @@
 package com.xixizai.personalblogwebsite.service;
 
+import com.xixizai.personalblogwebsite.pojo.result.Result;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ArticleCategoryService {
+
+    //获取所有文章分类
+    Result getAllArticleCategories();
 }
