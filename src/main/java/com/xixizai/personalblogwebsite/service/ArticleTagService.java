f47ede38754fe51c927acd79f90eebb0691171e2
Zyyyue <183977828+Zyyyue@users.noreply.github.com>
2026-03-27 12:00:00 +0800

    添加标签的接口

--- src/main/java/com/xixizai/personalblogwebsite/service/ArticleTagService.java
+++ src/main/java/com/xixizai/personalblogwebsite/service/ArticleTagService.java
@@ -1,7 +1,9 @@
 package com.xixizai.personalblogwebsite.service;
 
 import com.sun.org.apache.xalan.internal.xsltc.cmdline.getopt.GetOptsException;
+import com.xixizai.personalblogwebsite.exception.AddOperationException;
 import com.xixizai.personalblogwebsite.exception.PassedParameterException;
+import com.xixizai.personalblogwebsite.pojo.dto.ArticleTagDTO;
 import com.xixizai.personalblogwebsite.pojo.result.Result;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,7 @@ import org.springframework.stereotype.Service;
 public interface ArticleTagService {
     //获取所有标签
     Result getAllArticleTags() throws PassedParameterException, GetOptsException;
+
+    //添加标签
+    Result addArticleTag(ArticleTagDTO articleTagDTO) throws PassedParameterException, AddOperationException;
 }
