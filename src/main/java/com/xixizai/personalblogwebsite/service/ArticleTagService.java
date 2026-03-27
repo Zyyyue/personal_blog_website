2510707d29d4dc19013c8bebabf240fddb02a1e5
Zyyyue <183977828+Zyyyue@users.noreply.github.com>
2026-03-27 12:00:00 +0800

    获取所有标签的接口

---
+++ src/main/java/com/xixizai/personalblogwebsite/service/ArticleTagService.java
@@ -0,0 +1,12 @@
+package com.xixizai.personalblogwebsite.service;
+
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.getopt.GetOptsException;
+import com.xixizai.personalblogwebsite.exception.PassedParameterException;
+import com.xixizai.personalblogwebsite.pojo.result.Result;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ArticleTagService {
+    //获取所有标签
+    Result getAllArticleTags() throws PassedParameterException, GetOptsException;
+}
