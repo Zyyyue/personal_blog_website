b5a16cd096df84fd469f6abce236b3010a54deab
Zyyyue <183977828+Zyyyue@users.noreply.github.com>
2026-03-27 12:00:00 +0800

    批量删除文章分类，这里我进行了判空，判断id是否存在，id判重，然后处理了传来的id如果有和没有或者错误

--- src/main/java/com/xixizai/personalblogwebsite/constant/MessageConstant.java
+++ src/main/java/com/xixizai/personalblogwebsite/constant/MessageConstant.java
@@ -35,4 +35,5 @@ public class MessageConstant {
     public static final String CANCLE_OR_NOT_TOP_ARTICLE_FALISURE = "置顶或取消置顶文章失败";
     public static final String ADD_OPERATION_FAILSURE = "添加操作失败";
     public static final String UPDATE_ARTICLES_CATEGORIES_FAILSURE = "更新文章分类失败";
+    public static final String BATCH_DELETE_ARTICLE_CATEGORIES_FAILSURE = "批量删除文章失败";
 }
---
+++ src/main/java/com/xixizai/personalblogwebsite/exception/BatchDeleteArticleCategoriesException.java
@@ -0,0 +1,13 @@
+package com.xixizai.personalblogwebsite.exception;
+
+public class BatchDeleteArticleCategoriesException extends Exception{
+
+    public BatchDeleteArticleCategoriesException(){
+
+    }
+
+    public BatchDeleteArticleCategoriesException(String msg){
+        super(msg);
+    }
+
+}
