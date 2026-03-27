35f289427c750e1a43f350664eb322dbc329dfac
Zyyyue <183977828+Zyyyue@users.noreply.github.com>
2026-03-27 12:00:00 +0800

    获取所有文章分类

--- src/main/java/com/xixizai/personalblogwebsite/controller/admin/ArticleCategoryController.java
+++ src/main/java/com/xixizai/personalblogwebsite/controller/admin/ArticleCategoryController.java
@@ -1,9 +1,27 @@
 package com.xixizai.personalblogwebsite.controller.admin;
 
+import com.xixizai.personalblogwebsite.pojo.result.Result;
+import com.xixizai.personalblogwebsite.service.ArticleCategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController("adminArticleCategoryController")
 @RequestMapping("/admin/articleCategory")
 public class ArticleCategoryController {
+
+    @Resource
+    private ArticleCategoryService articleCategoryService;
+
+    /**
+     * 获取所有分类
+     * @return
+     */
+    @GetMapping()
+    public Result getArticleCategories(){
+        return articleCategoryService.getAllArticleCategories();
+    }
+
 }
--- src/main/java/com/xixizai/personalblogwebsite/service/Impl/ArticleCategoryServiceImpl.java
+++ src/main/java/com/xixizai/personalblogwebsite/service/Impl/ArticleCategoryServiceImpl.java
@@ -1,8 +1,35 @@
 package com.xixizai.personalblogwebsite.service.Impl;
 
+import com.xixizai.personalblogwebsite.mapper.ArticleCategoryMapper;
+import com.xixizai.personalblogwebsite.pojo.entity.ArticleCategories;
+import com.xixizai.personalblogwebsite.pojo.result.Result;
 import com.xixizai.personalblogwebsite.service.ArticleCategoryService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @Service
 public class ArticleCategoryServiceImpl implements ArticleCategoryService {
+
+
+    @Resource
+    private ArticleCategoryMapper articleCategoryMapper;
+
+    /**
+     * 获取所有文章分类
+     * @return
+     */
+    @Override
+    public Result getAllArticleCategories() {
+        List<ArticleCategories>list= articleCategoryMapper.getAllArticleCategories();
+        //这里判空一下
+        if(list==null||list.isEmpty()){
+            return Result.error("文章分类表中没有文章分类，获取失败");
+        }else{
+            return Result.success(list);
+        }
+    }
+
+
 }
