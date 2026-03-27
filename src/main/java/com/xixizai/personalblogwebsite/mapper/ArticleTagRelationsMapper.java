e2e74ff5239e33d41c06d7a1304274e266214e18
Zyyyue <183977828+Zyyyue@users.noreply.github.com>
2026-03-27 12:00:00 +0800

    woc了太鸡巴搞了，这个创建文章这个接口我找错找了半天，结果就是我测试使用的{                                           "title": "36",                                           "slug": "三国演义",                                           "summary": "文章摘要",                                           "coverImage": "/img/cover.jpg",                                           "contentMarkdown": "# 成为神的代价\n\n这是一个测试内容",                                           "contentHtml": "<h1>成为神的代价</h1><p>这是一个测试内容</p>",                                           "categoryId": 2,                                           "isPublished": 0,                                           "isTop": 0,                                           "tagIds":[1,2,3]
                                          }
    中使用的tagId和dto中的tagIds的名字匹配不上,端点调试一下其实就可以了，但是我直接ai找了半天，还是要会调试啊，不要畏惧，可惜了cc烧了1美刀!
    
    然后这里我创建文章这个接口主要也和前面一样也是重写了一下多表查询，然后也加了一个事务回滚，确保一致性

--- src/main/java/com/xixizai/personalblogwebsite/mapper/ArticleTagRelationsMapper.java
+++ src/main/java/com/xixizai/personalblogwebsite/mapper/ArticleTagRelationsMapper.java
@@ -3,6 +3,7 @@ package com.xixizai.personalblogwebsite.mapper;
 import com.xixizai.personalblogwebsite.pojo.dto.ArticleDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -19,4 +20,20 @@ public interface ArticleTagRelationsMapper {
      * @param tagIds
      */
     void updateArticleTagsAndRelations(@Param("tagIds") List<Long> tagIds, @Param("id") Long id);
+
+    /**
+     * 创建文章标签关系表中的数据
+     * @param tagIds
+     * @param articleId
+     */
+    void createArticleTagsAndRelations(@Param("tagIds1") List<Long> tagIds, @Param("id1") Long articleId);
+
+    /**
+     * 根据slug查找，因为slug是用unique约束的
+     * @param slug
+     * @return
+     */
+    @Select("select * from articles where slug=#{slug}")
+    ArticleDTO findBySlug(String slug);
+
 }
