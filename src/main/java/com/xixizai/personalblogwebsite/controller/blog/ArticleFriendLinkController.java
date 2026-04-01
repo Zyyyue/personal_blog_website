1a054b0f43dac3f83019f33dfd7f70c488901039
Zyyyue <3467884929@qq.com>
2026-04-01 23:13:52 +0800

    blogArticleFriendLink获取可见友链

--- src/main/java/com/xixizai/personalblogwebsite/controller/blog/ArticleFriendLinkController.java
+++ src/main/java/com/xixizai/personalblogwebsite/controller/blog/ArticleFriendLinkController.java
@@ -1,5 +1,28 @@
 package com.xixizai.personalblogwebsite.controller.blog;
 
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.getopt.GetOptsException;
+import com.xixizai.personalblogwebsite.pojo.result.Result;
+import com.xixizai.personalblogwebsite.service.ArticleFriendLinkService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/blog/friendLink")
 public class ArticleFriendLinkController {
+
+    @Resource
+    private ArticleFriendLinkService articleFriendLinkService;
+
+    /**
+     * 获取可见友链
+     * @return
+     */
+    @GetMapping()
+    public Result getFriendLink() throws GetOptsException {
+        return articleFriendLinkService.getFriendLink();
+    }
+
 }
